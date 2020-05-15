2c3889d371b3f29b0c5a7248d22a0296dc36bad1
JBris <james.a.bristow.contact@gmail.com>
2020-05-16 04:51:41 +1200

    Adding cross validation for HDBSCAN

--- scripts/user/models/hdbscan.py
+++ scripts/user/models/hdbscan.py
@@ -3,9 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import warnings  
-import hdbscan
+from hdbscan import HDBSCAN as BaseHDBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, train_test_split, TimeSeriesSplit
 
+#The HDBSCAN class is missing a few methods. So we have to add some method wrappers.
+class HDBSCAN(BaseHDBSCAN):
+  def predict(self, X):
+    return self.fit_predict(X)
+    
+  def decision_function(self, X):
+    return self.outlier_scores_
+    
 def pivot(df):
   channel_df = pd.pivot_table(
     df,
@@ -33,32 +42,41 @@ def load_data():
   return pivot(df), pivot(df_20)
 
 def fit_model(df):
-  channel = 20
-  model = hdbscan.HDBSCAN(
-    algorithm='best', 
-    #alpha=1.0, 
-    approx_min_span_tree=True,
-    gen_min_span_tree=False, 
-    leaf_size = 50, 
-    #memory=Memory(cachedir=None),
-    metric='euclidean', 
-    min_cluster_size= 4 * 24 * 14, 
-    min_samples= 4 * 24, 
-    #p=None,
-    core_dist_n_jobs = -1,
-    cluster_selection_method = "leaf",
-    cluster_selection_epsilon = 0.15,
-  )
-  
-  def model_channel(i):
-    model.fit(df.iloc[:,i:i+1].values)
-    pred = model.fit_predict(df.iloc[:,i:i+1].values)
-      
+  def model_channel(i, model):
+    #scoring
+    tuned = {
+      'algorithm':['best'], 
+      'approx_min_span_tree':[True],
+      'gen_min_span_tree':[False],
+      'leaf_size':[50],
+      'metric':['euclidean'],
+      'min_cluster_size':[4 * 24 * 14],
+      'leaf_size':[50],
+      'min_samples':[4 * 24],
+      'cluster_selection_method':['leaf'],
+      'cluster_selection_epsilon':[0.15]
+    }
+    
+    X_train, X_test = train_test_split(df.iloc[:,i:i+1].values, train_size=0.5, random_state=1234, shuffle=True)
+    tss = TimeSeriesSplit(n_splits=10)
+    model = GridSearchCV(
+      estimator=model, 
+      param_grid=tuned, 
+      cv=tss,
+      scoring=('r2', 'neg_mean_squared_error'),
+      refit='neg_mean_squared_error',
+    )
+    
+    model.fit(X_train, X_test)
+    
+    print("GridCV", model.cv_results_)
+    pred = model.predict(df.iloc[:,i:i+1].values)
+    
     test_df = pd.DataFrame()
     test_df['date_time'] = df.index
     test_df = test_df.sort_values(by='date_time')
   
-    test_df['score']=model.outlier_scores_
+    test_df['score']=model.decision_function(df.iloc[:,i:i+1].values)
     test_df.score.fillna(0, inplace=True)
     test_df['usage']=df.iloc[:,i:i+1].values
     test_df['anomaly'] = pred
@@ -74,8 +92,25 @@ def fit_model(df):
       
     test_df.to_csv(f"~/data/hdbscan_channel_{channel_id}.csv", index=False)
     return test_df
-    
-  test_df = model_channel(channel)
+  
+  channel = 20
+  model = HDBSCAN(
+    algorithm='best', 
+    #alpha=1.0, 
+    approx_min_span_tree=True,
+    gen_min_span_tree=False, 
+    leaf_size = 50, 
+    #memory=Memory(cachedir=None),
+    metric='euclidean', 
+    min_cluster_size= 4 * 24 * 14, 
+    min_samples= 4 * 24, 
+    #p=None,
+    core_dist_n_jobs = -1,
+    cluster_selection_method = "leaf",
+    cluster_selection_epsilon = 0.15,
+  )  
+  
+  test_df = model_channel(channel, model)
   print(test_df['anomaly'].value_counts())
   print(test_df)
   
