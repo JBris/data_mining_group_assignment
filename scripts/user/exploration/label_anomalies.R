512c5b59df89a548e22431a82c4a320915a9b3e5
JBris <james.a.bristow.contact@gmail.com>
2020-06-13 18:15:35 +1200

    Adding iForest cross-validation

--- scripts/user/exploration/label_anomalies.R
+++ scripts/user/exploration/label_anomalies.R
@@ -11,8 +11,10 @@ data <- read_csv("data/final_processed.csv") %>%
   group_by(channel_id) %>%
   arrange(date_time)
 
+# SH-ESD Test
+  
 fifteen_min_anomalized <- data %>%
-  group_by(channel_id) %>%
+  group_by(site) %>%
   time_decompose(
     usage, 
     frequency = "1 day", 
@@ -22,14 +24,19 @@ fifteen_min_anomalized <- data %>%
   ) %>%
   anomalize(
     remainder, 
-    method="iqr", # or gesd
+    method=" gesd", # or iqr
     alpha = 0.05
   ) %>% 
   time_recompose()
 
 fifteen_min_anomalized %>%
+  filter(channel_id %in% 20) %>%
+  plot_anomalies(ncol = 3, alpha_dots = 0.05, time_recomposed = T)
+
+fifteen_min_anomalized %>%
   mutate(
-    anomaly = ifelse(anomaly== "Yes", 1, 0)
+    anomaly = ifelse(anomaly %in% "Yes" | missing %in% 1, 1, 0)
   ) %>%
   select(channel_id, date_time, usage, site, missing, month, day, hour, minute, anomaly) %>%
-  write_csv("data/labelled_final_processed.csv")
+  write_csv("data/labelled_processed.csv")
+
---
+++ scripts/user/models/isolation_forest_cross_val.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings  
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, f1_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV, train_test_split, TimeSeriesSplit
+
+def pivot(df):
+  channel_df = pd.pivot_table(
+    df,
+    values = ['usage', 'anomaly'],
+    index = 'date_time',
+    columns = 'channel_id'
+  )
+  channel_df.fillna(0, inplace=True)
+  print(channel_df)
+  return channel_df
+
+def load_data():
+  df = pd.read_csv(
+        'data/labelled_final_processed.csv',
+         parse_dates = ['date_time'],
+         infer_datetime_format = True,
+         dayfirst = True,
+      )
+      
+  print(df)
+  
+  #Handle site 20 here
+  df_20 = df.loc[df.site == 20] 
+  df = df.loc[df.site != 20] 
+  return df, df_20
+
+
+def fit_model(df):
+  def model_channel(i):
+    df_model = df.loc[df.site == i]
+    
+    #Produce anomaly labels
+    df_model.loc[df_model['anomaly'] == 1, 'anomaly'] = -1
+    df_model.loc[df_model['anomaly']== 0, 'anomaly'] = 1
+    df_model = pivot(df_model)
+    scaler = StandardScaler()
+    scaler.fit_transform(df_model.usage)
+    print(df_model)
+    df_model.loc[df_model.anomaly.isin([-1]).any(axis=1), 'anomaly'] = -1
+    labels = df_model.iloc[:,0]
+    print(labels)
+    
+    #scoring
+    param_grid = {
+      'n_estimators':[50, 100, 150, 200],
+      'max_samples':['auto'],
+      'contamination':[.01, .05, .1, .15, .2],
+      'max_features':[.5, 1.0],
+      'bootstrap':[False],
+      'random_state':[1234],
+      'warm_start':[False],
+    }
+    
+    X_train, X_test, Y_train, Y_test = train_test_split(
+      df_model.usage.values, 
+      labels.values, 
+      train_size=0.75, 
+      shuffle=False
+    )
+    cv = TimeSeriesSplit(n_splits=3)
+    clf = GridSearchCV(
+      estimator=IsolationForest(),
+      param_grid=param_grid,
+      cv=cv,
+      scoring=['neg_mean_squared_error', 'f1'],
+      refit='neg_mean_squared_error'
+    )
+    clf.fit(X_train, Y_train)
+    
+    scoring_params = ["mean_fit_time", "mean_score_time", "mean_test_f1", "rank_test_f1", "mean_test_neg_mean_squared_error", 
+      "rank_test_neg_mean_squared_error"]
+    scoring_params_df = [ pd.DataFrame(clf.cv_results_["params"]) ]
+    for param in scoring_params: 
+      scoring_params_df.append( pd.DataFrame(clf.cv_results_[param], columns=[param]) )
+    tuning_results = pd.concat(scoring_params_df, axis = 1)
+    print(tuning_results)
+    tuning_results.to_csv(f"~/data/isolation_forest_tuning_results.csv", index=False)
+    Y_hat = clf.predict(X_test)
+    pred_results = {
+      'mse' : mean_squared_error(Y_test, Y_hat),
+      'f1' : f1_score(Y_test, Y_hat),
+      'roc_auc': roc_auc_score(Y_test, clf.decision_function(X_test))
+    }
+    print(pred_results)
+    
+  site = 14
+  model_channel(site)
+
+df, df_20 = load_data()
+fit_model(df)
